8a44651a86e6ccf343197f6880737aa1cd62d86c
Ensar Erdag <ebhan96@hotmail.com>
2020-12-25 15:57:33 +0300

    Add for loop for multiple files without time_log variable

--- plink_.bat
+++ plink_.bat
@@ -16,10 +16,9 @@ shift
 set component=%9
 echo starting...
 
-
-echo FOR /F "delims=" %%i IN ('plink -hostkey %hostkey% -batch siebel@%serv_ip% -pw %servpw% "cd %path_unix_log%;echo $(grep -l %keyword% %component:"=%*.log);" "exit"') DO set file_name=%%i
-FOR /F "delims=" %%i IN ('plink -hostkey %hostkey% -batch siebel@%serv_ip% -pw %servpw% "cd %path_unix_log%;echo $(grep -l %keyword% %component:"=%*.log);" "exit"') DO set file_name=%%i
-echo file_name: %file_name%
+echo FOR /F "tokens=*" %%i IN ('plink -hostkey %hostkey% -batch siebel@%serv_ip% -pw %servpw% "cd %path_unix_log%;echo $(grep -l %keyword% %component:"=%*.log);" "exit"') DO ( call set "files=%%files%% %%i")
+FOR /F "tokens=*" %%i IN ('plink -hostkey %hostkey% -batch siebel@%serv_ip% -pw %servpw% "cd %path_unix_log%;echo $(grep -l %keyword% %component:"=%*.log);" "exit"') DO ( call set "files=%%files%%%%i")
+echo files: %files%
 
 IF %action%=="OPEN_LOG" (
 	echo Opening log %%5 on server: %server_name%
@@ -38,14 +37,14 @@ IF %action%=="REQUEST_LOG" (
 	set hostkey=%hostkey:"=%
 	set path_unix_log=%path_unix_log:"=%
 	set path_log=%path_log:"=%
-	set file_name=%file_name:"=%
-	set time_log=%time_log:"=%
-	echo "C:\Program Files (x86)\WinSCP\WinSCP.exe" /command "option batch on" "option confirm off" "open -hostkey="%hostkey%" siebel:%servpw%@%serv_ip%" "get %path_unix_log%/%file_name% E:\LogCopyAutomation\%time_log%_%file_name%" "/log=%path_log%\LogCopy_%time_log%_%file_name%" "exit"
-		"C:\Program Files (x86)\WinSCP\WinSCP.exe" /command "option batch on" "option confirm off" "open -hostkey="%hostkey%" siebel:%servpw%@%serv_ip%" "get %path_unix_log%/%file_name% E:\LogCopyAutomation\%time_log%_%file_name%" "/log=%path_log%\LogCopy_%time_log%_%file_name%" "exit"
-	echo %time_log:~1,-1%_%file_name% copied successfully to E:/ drive.
-	timeout /t 3
+	set files=%files:"=%
+	echo "C:\Program Files (x86)\WinSCP\WinSCP.exe" /command "option batch on" "option confirm off" "open -hostkey="%hostkey%" siebel:%servpw%@%serv_ip%" "get %path_unix_log%/%files% E:\LogCopyAutomation\%files%" "/log=%path_log%\LogCopy_%files%" "exit"
+		"C:\Program Files (x86)\WinSCP\WinSCP.exe" /command "option batch on" "option confirm off" "open -hostkey="%hostkey%" siebel:%servpw%@%serv_ip%" "get %path_unix_log%/%files% E:\LogCopyAutomation\%files%" "/log=%path_log%\LogCopy_%files%" "exit"
+	echo %files% copied successfully to E:/ drive.
 	E:
 	cd E:\LogCopyAutomation\
-	"C:\Program Files\7-Zip\7z.exe" a %time_log:~1,-1%_%file_name:~0,-4%.zip E:\LogCopyAutomation\%time_log:~1,-1%_%file_name% -mx5
-	del %time_log:~1,-1%_%file_name%
+	echo "C:\Program Files\7-Zip\7z.exe" a %keyword%.zip %files% -mx5
+	"C:\Program Files\7-Zip\7z.exe" a %keyword%.zip %files% -mx5
+	exit
+	del %files%
 )
