60d574b774fcadd1418023b3bb23839cb1dc20a6
Ensar Erdag <ebhan96@hotmail.com>
2021-01-13 08:36:54 +0300

    Add logging to log_status.py

--- log_status.py
+++ log_status.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from json import load, loads, dumps
+from logging import FileHandler, basicConfig, info, error, DEBUG
 from time import ctime
 
 from flask import request, Flask
@@ -14,21 +15,26 @@ tl = Timeloop()
 
 @tl.job(interval=timedelta(minutes=5))
 def update_log_level_status():
-    print("\n" + ctime())
+    info("\n" + ctime())
     for machine_no in [str(x) for x in range(1, 15)]:
-        with open("static/data/servers.json") as f:
-            servers = load(f)
-        last_update = datetime.fromtimestamp(int(servers[int(machine_no) - 1]["log_level_last_updated"].split('.')[0]))
-        time_difference = divmod((datetime.now() - last_update).seconds, 60)[1]
-        if servers[int(machine_no) - 1]["log_level_status"] == '5' and \
-            time_difference >= 0:
-            with app.test_request_context('/', json=dumps({"machine_no": machine_no, "isAdm": False, "Server_action": "close_log"})):
-                siebel = Siebel(req_data=loads(request.json))
-            siebel.change_log_level(Change_log_action.DECREASE)
-            print(f"Log level on machine #{machine_no} has been changed to 0 due to 30+ minutes of continuous activeness since: {last_update}.")
-        else:
-            print(f"Skipping. It's been {time_difference} minutes since the last check on machine #{machine_no}.")
-
+        try:
+            with open("static/data/servers.json") as f:
+                servers = load(f)
+            last_update = datetime.fromtimestamp(int(servers[int(machine_no) - 1]["log_level_last_updated"].split('.')[0]))
+            time_difference = divmod((datetime.now() - last_update).seconds, 60)[1]
+            if servers[int(machine_no) - 1]["log_level_status"] == '5' and \
+                time_difference >= 0:
+                with app.test_request_context('/', json=dumps({"machine_no": machine_no, "isAdm": False, "Server_action": "close_log"})):
+                    siebel = Siebel(req_data=loads(request.json))
+                siebel.change_log_level(Change_log_action.DECREASE)
+                info(f"Log level on machine #{machine_no} has been changed to 0 due to 30+ minutes of continuous activeness since: {last_update}.")
+            else:
+                info(f"Skipping. It's been {time_difference} minutes since the last check on machine #{machine_no}.")
+        except:
+            error(f"Error on machine #{machine_no}.", exc_info=True)
 
 if __name__ == '__main__':
+    basicConfig(handlers=[FileHandler(encoding='utf-8', filename='log_status.log', mode='w')],
+                level=DEBUG,
+                format=u'%(levelname)s - %(name)s - %(asctime)s: %(message)s')
     tl.start(block=True)
